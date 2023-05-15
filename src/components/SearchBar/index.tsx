9e99e79cd7401ae2ad4d4abcddbd1ba087e4cdd9
matipeco <matiaspecorale1995@gmail.com>
2023-05-14 21:13:06 -0300

    actualizando search, chequea si la ciudad ya fue buscada

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,28 +1,47 @@
 import { useState, FormEvent, ChangeEvent, FunctionComponent } from "react";
-import style from './index.module.css'
+import { Weather } from "../../App";
+import style from "./index.module.css";
 
 type Props = {
-    weather: (city: string) => Promise<void>;
-}
+  weather: (city: string) => Promise<void>;
+  citiesWeather: Weather[];
+};
 
-export const SearchBar: FunctionComponent<Props> = ({ weather }) => {
-    const [input, setInput] = useState("");
+export const SearchBar: FunctionComponent<Props> = ({
+  weather,
+  citiesWeather,
+}) => {
+  const [input, setInput] = useState("");
 
-    const handleSubmit = (ev: FormEvent<HTMLFormElement>) => {
-        ev.preventDefault();
-        weather(input);
-        setInput("");
+  const handleSubmit = (ev: FormEvent<HTMLFormElement>) => {
+    const cityFound = citiesWeather.find(
+      (c) => c.name.toLowerCase() === input.toLowerCase()
+    );
+
+    if (cityFound) {
+      alert("La ciudad ya ha sido buscada!");
+      ev.preventDefault();
+      setInput("");
+    } else {
+      ev.preventDefault();
+      weather(input);
+      setInput("");
     }
+  };
 
-    const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
-        setInput(ev.target.value.toLowerCase());
-    }
+  const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    setInput(ev.target.value.toLowerCase());
+  };
 
-    return (
-        <form className={style.searchBarBackground} onSubmit={handleSubmit}>
-            <input type="text" value={input} onChange={handleChange} placeholder='Search city...' />
-            <button className={style.button}></button>
-        </form >
-    )
-
-}
+  return (
+    <form className={style.searchBarBackground} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={input}
+        onChange={handleChange}
+        placeholder="Search city..."
+      />
+      {/* <button className={style.button}></button> */}
+    </form>
+  );
+};
