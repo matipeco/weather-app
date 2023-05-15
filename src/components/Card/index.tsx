e4c45db4e70f80ab1b388764db67fe82ca8a253a
matipeco <matiaspecorale1995@gmail.com>
2023-05-14 21:12:25 -0300

    actualizando componente Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,25 +1,45 @@
-import { Weather } from '../../App'
-import { Dispatch, FunctionComponent } from 'react'
-import { StyledCard } from './styles'
+import { Weather } from "../../App";
+import { Dispatch, FunctionComponent } from "react";
+import { StyledCard } from "./styles";
 
+export type Props = {
+  city: Weather | undefined;
+  citiesWeather: Weather[];
+  setCitiesWeather: Dispatch<React.SetStateAction<Weather[]>>;
+};
 
-type Props = {
-    cityWeather: Weather | undefined
-    setFlag: Dispatch<React.SetStateAction<boolean>>
-}
+const Card: FunctionComponent<Props> = ({
+  city,
+  citiesWeather,
+  setCitiesWeather,
+}) => {
+  const handleDelete = (id: number | undefined) => {
+    setCitiesWeather(citiesWeather.filter((city) => city.id !== id));
+  };
 
-const Card: FunctionComponent<Props> = ({ cityWeather, setFlag }) => {
-    return (
-        <StyledCard>
-            <div className='card-container'>
-                <button className='card-button' onClick={() => setFlag(false)}>x</button>
-                <h3>{cityWeather?.sys.country}</h3>
-                <h3>{cityWeather?.name}</h3>
-                <p><span>Temp.Actual: </span>{cityWeather?.main.temp}</p>
-                <p> <span>Clima: </span>{cityWeather?.weather[0].description}</p>
-                <p> <span>Humedad: </span>% {cityWeather?.main.humidity}</p>
-            </div>
-        </StyledCard>
-    )
-}
-export default Card
+  return (
+    <StyledCard>
+      <div className="card-container">
+        <button className="card-button" onClick={() => handleDelete(city?.id)}>
+          x
+        </button>
+        <h3>{city?.sys.country}</h3>
+        <h3>{city?.name}</h3>
+        <p>
+          <span>Temp.Actual: </span>
+          {city?.main.temp}
+        </p>
+        <p>
+          {" "}
+          <span>Clima: </span>
+          {city?.weather[0].description}
+        </p>
+        <p>
+          {" "}
+          <span>Humedad: </span>% {city?.main.humidity}
+        </p>
+      </div>
+    </StyledCard>
+  );
+};
+export default Card;
