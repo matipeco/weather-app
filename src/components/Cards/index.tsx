3f7a40a325c2b5c453c59b82614ef34dab9dc87f
matipeco <matiaspecorale1995@gmail.com>
2023-05-14 21:11:53 -0300

    creando componente Cards, reordenando codigo

---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,27 @@
+import { Dispatch, FunctionComponent } from "react";
+import { Weather } from "../../App";
+import Card from "../Card";
+import { StyledCards } from "./style";
+
+type Props = {
+  citiesWeather: Weather[] | undefined;
+  setCitiesWeather: Dispatch<React.SetStateAction<Weather[]>>;
+};
+
+export const Cards: FunctionComponent<Props> = ({
+  citiesWeather,
+  setCitiesWeather,
+}) => {
+  return (
+    <StyledCards>
+      {citiesWeather?.map((city) => (
+        <Card
+          key={city.id}
+          city={city}
+          citiesWeather={citiesWeather}
+          setCitiesWeather={setCitiesWeather}
+        />
+      ))}
+    </StyledCards>
+  );
+};
---
+++ src/components/Cards/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledCards = styled.div`
+  border: 2px solid yellowgreen;
+  display: flex;
+  align-items: center;
+`;
